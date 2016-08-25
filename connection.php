d947fae2957672333d714fec3aaa76b4c03a67dc
Luc Princen <luc@chefduweb.nl>
2016-08-25 22:43:37 +0200

    Connection class scaffoleded

--- connection.php
+++ connection.php
@@ -1,19 +1,76 @@
 <?php
 
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
 
-// The directory separator.
-defined('DS') ? DS : define('DS', DIRECTORY_SEPARATOR);
+// Add this packit to a global array:
+$GLOBAL['packits'][ plugin_basename( __FILE__ ) ] = '57bf2bc37a5f0';
 
 
+// Initiate the class once:
 if( !class_exists( 'AlpackitUpdateController' ) ){
 
-    //our update-checker:
+    // Alpackits update-checker:
     class AlpackitUpdateController{
 
+        /**
+         * Base url
+         *
+         * @var string
+         */
+        const BASE_URL = 'http://alpackit.dev';
+
+        /**
+         * Keeps the connection location
+         *
+         * @var string
+         */
+        protected $location = '/vendors/alpackit/connection.php';
+
+        /**
+         * The current plugin slug
+         *
+         * @var string
+         */
+        protected $slug;
+
+
+        /**
+         * Alpackit license token
+         *
+         * @var string
+         */
+        protected $license;
+
+
+        /**
+         * Constructor
+         */
         public function __construct()
         {
-            echo __FILE__;
+            //get the plugin slug:
+            $this->slug = $this->makeSlug( plugin_basename( __FILE__ ) );
+            $this->license = get_option( $this->slug.'-license', '' );
+
+            $this->setEvents();
+        }
+
+        /**
+         * Set events for updates and license checks
+         *
+         */
+        public function setEvents()
+        {
+
+        }
+
+        /**
+         * Generate the plugin slug
+         *
+         * @return string
+         */
+        private makeSlug(){
+            return str_replace( $this->location, '', plugin_basename( __FILE__ ) );
         }
 
     }
